2c8db039ca1b58d4114c4172556de7751f5cb94d
anaritaasp <ritaa3008@gmail.com>
2022-11-13 11:56:38 +0000

    trying to make an adjency matrix out of the given circuit

--- racevector.py
+++ racevector.py
@@ -21,15 +21,17 @@ def listaToMatriz(arr):
     #print (x)
     return [[instance for instance in sublist if not instance.isspace()] for sublist in x] #remove os espaços em branco da matriz
 
+    
 #função que retorna uma lista com os indices das posicoes da matriz para onde o jogador se pode movimentar (todas menos 'X') -- ele pode andar por 'P', '-' e 'F'
 def posicoesValidas(arr):
     matriz=listaToMatriz(arr)
+    lista= matriz[0]
+    #print (lista)
     posicoesOk =[]
-    for i in range (len(matriz)):
-        for j in range (len(matriz)):
+    for i in range (len(matriz)): #linhas
+        for j in range (len(lista)): #colunas
             if (matriz[i][j] != 'X'): posicoesOk.append([i,j])
-    #print (posicoesOk)
-    #print (matriz)
+    print (posicoesOk)
     return posicoesOk
 
 #função que verifica se a proxima posição é adjacente à atual
@@ -56,25 +58,48 @@ def movimentoJogadorOK(x_novo, y_novo, x_velho,y_velho):
 #array 2d que guarda o custo de cada posição
 def custoPos():
     matriz=listaToMatriz(arr)
+    lista = matriz[0]
     custos =[]
     for i in range (len(matriz)):
         custo=[]
-        for j in range (len(matriz)):
+        for j in range (len(lista)):
             if (matriz[i][j] != 'X'): 
                 custo.append(1)
             else:  
                 custo.append(25)
         custos.append(custo)
-    #print(custos)
     return(custos)
-
-
  
-    
+def matrizToLista():
+    matriz=listaToMatriz(arr)
+    lista = matriz[0]
+    listanew =[]
+    for i in range (len(matriz)):
+        for j in range (len(lista)):
+                e= matriz[i][j]
+                listanew.append(e)
+    return listanew
+
+#nao tá correto
+def adj():
+    matriz=listaToMatriz(arr)
+    sizeLinha=len(matriz[0])# tamanho duma linha
+    sizeColuna = len(matriz)#tamanho de uma coluna
+    lista = custoPos()
+    listaA=[]
+    print (sizeLinha)
+    for i in range (sizeColuna):
+        for j in range ( sizeLinha):
+            if (i<sizeLinha): #para a primeira linha - pq o 0%nr == 0
+                tuplo=[(i,j), (i,j+1), lista[i][j+1]]
+                print (tuplo)
+            if (i> sizeLinha and i%sizeLinha != 0):#para a partir da segunda linha
+                tuplo=[(i,j), (i,j+1), lista[i][j+1]]
+                print (tuplo)
+            
 #imprimeCircuito(arr)
 #print(listaToMatriz(arr))
 #print(posicoesValidas(arr))
-#print(movimentoJogadorOK(1,2,0,0))
+#print(movimentoJogadorOK(3,2,3,1))
 #custoPos()
-
-
+#adj()
